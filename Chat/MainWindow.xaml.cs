0404424d1a00c5c2608c1eefe8128b4c45ebddbf
Yeusint <hxx770308@163.com>
2023-08-19 14:04:17 +0800

    6

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -22,17 +22,53 @@ namespace Chat
     {
         public MainWindow()
         {
+            /*
+             <ListViewItem Padding="0">
+                    <StackPanel Orientation="Horizontal" HorizontalAlignment="Center">
+                        <Ellipse Cursor="Hand" Width="50" Height="50">
+                            <Ellipse.Fill>
+                                <ImageBrush  ImageSource="res/icon.png" />
+                            </Ellipse.Fill>
+                        </Ellipse>
+                        <StackPanel Orientation="Vertical" VerticalAlignment="Center" Margin="5 0">
+                            <TextBlock FontSize="15" Foreground="Black" Text="你没事吧" />
+                            <TextBlock  Margin="0 2 0 0" FontSize="12" Text="我说了个114514" />
+                        </StackPanel>
+                    </StackPanel>
+                </ListViewItem>
+             */
             InitializeComponent();
-        }
-
-        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+            List<ListViewItem> d = new List<ListViewItem>()
+            {
+                new ListViewItem(){
+                    Padding=new Thickness() { Bottom=0,Top=0, Right=0, Left=0},
+                    Content=new StackPanel(){
+                        Orientation=Orientation.Horizontal,
+                        HorizontalAlignment=HorizontalAlignment.Center,
+                        
+                    }
+                }
+            };
+            fl.ItemsSource = d;
+            fl.DisplayMemberPath = "Name";
+            Binding b = new Binding("SelectedItem.id") { Source = this.fl };
+            tb.SetBinding(TextBlock.TextProperty, b);
         }
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             fl.Height = bg.ActualHeight;
+        }
+
+        private void fl_Selected(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(sender.ToString());
+        }
+
+        public class User
+        {
+            public int id { get; set; }
+            public string Name { get; set; }
         }
     }
 }
